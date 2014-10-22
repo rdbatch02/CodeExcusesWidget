7cfb44fb6edbe351fe166eb13bdf8cf325cfd727
Ryan Batchelder <rb688@nau.edu>
2014-10-22 07:55:33 -0700

    Initial Commit

---
+++ Code Excuses/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Code Excuses
+//
+//  Created by Ryan Batchelder on 10/22/14.
+//  Copyright (c) 2014 Ryan Batchelder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
